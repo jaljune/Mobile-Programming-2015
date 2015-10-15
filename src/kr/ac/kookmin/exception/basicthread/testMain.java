b04d946116ceb0e4831369712493966074439bf3
jaljune <astrayakma@naver.com>
2015-10-15 12:59:31 +0900

    7-0

---
+++ src/kr/ac/kookmin/exception/basicthread/testMain.java
@@ -0,0 +1,40 @@
+package kr.ac.kookmin.exception.basicthread;
+
+public class testMain {
+	
+	public static BankAccount account = new BankAccount();
+	public static void main (String []args) {
+		
+	
+		Thread depositMan = new Thread() {
+            public void run() {
+                while (true) {
+                	try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                      	account.add(10); 	// 10 만큼 입금
+                }
+            }
+        };
+        Thread withdrawMan = new Thread() {
+            public void run() {
+                while (true) {
+                        try {
+							Thread.sleep(500);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                          account.delete(10);  // 10 만큼 출금 
+                }
+            }
+        };
+        account.getBalance();
+        account.withDraw(10);
+        depositMan.start();
+        withdrawMan.start();
+	}
+}
